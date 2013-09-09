cff59d760a43e11fb2eb83eac0ac021925031a3e
Gabe Del <me@gabe.io>
2013-09-09 16:41:31 -0400

    added sales & sales2

---
+++ sales.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item book; // book
+    std::cin >> book; // book input
+    std::cout << book << std::endl; // book output
+    return 0;
+}
