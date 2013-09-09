80cd1d2bd85b4150b814a41fd0062294b97c1cbd
Gabe Del <me@gabe.io>
2013-09-09 17:36:36 -0400

    added full.cc to keep track of 'good' programatic code

---
+++ full.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+/*
+C++11 Notes for future development of C++ programs
+*/
+int main()
+{
+    
+    int var1 = 0;
+    
+    while(std::cin >> var1){
+        // do something while cin can be put into var1 (type check.)
+    }
+    
+    if(std::cin >> var1)
+        // only do if cin can be put into var1 (type check.)
+    
+    
+    if(error)
+    {
+        return -1; // return error
+    }
+    
+    return 0; // return okay
+    
+}
