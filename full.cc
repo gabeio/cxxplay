244e083b88f2405cb76c9c202147f1bd410838e2
Gabe Del <me@gabe.io>
2013-09-09 17:40:57 -0400

    added more comments for std::cerr and std::endl

--- full.cc
+++ full.cc
@@ -1,22 +1,27 @@
-#include <iostream>
+#include <iostream> // is only for programs that will be taking&putting via terminal (console apps)
 /*
 C++11 Notes for future development of C++ programs
 */
 int main()
 {
     
-    int var1 = 0;
+    int var1 = 0; // example var1 of type int
     
-    while(std::cin >> var1){
+    while(std::cin >> var1){ // in this case checks if cin is an int that can fit into var1
         // do something while cin can be put into var1 (type check.)
     }
     
-    if(std::cin >> var1)
-        // only do if cin can be put into var1 (type check.)
-    
-    
-    if(error)
+    if(std::cin >> var1) // in this case checks if cin is an int that can fit into var1
     {
+        // only do if cin can be put into var1 (type check.)
+    }
+    
+    bool error = false;
+    
+    if(error) // if error is true then stop program and show terminal that there was an error
+    {
+        std::cerr << "this will go to the error cout"
+            << std::endl; // std::endl ALWAYS REQUIRED AT END OF OUTPUTS TO FLUSH BUFFERS!!!!!
         return -1; // return error
     }
     
