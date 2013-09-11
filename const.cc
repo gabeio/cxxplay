f28c858c5819c87363b68d25ca123e09cd36582e
Gabe Del <me@gabe.io>
2013-09-11 03:08:49 -0400

    adding to const.cc

--- const.cc
+++ const.cc
@@ -5,6 +5,8 @@ int main()
     // const int i; // error not initialized.
     const int z = 5;
     
+    extern const bitSize = 512; // extern allows to be used extern(ally)...
+    
     // z = 0; // error can not change variable of a const
     
     return 0;
