6411c99f1d988e9200a218d845f3a7a1c241da3b
Gabe Del <me@gabe.io>
2013-09-11 03:07:05 -0400

    const.cc

---
+++ const.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    // const int i; // error not initialized.
+    const int z = 5;
+    
+    // z = 0; // error can not change variable of a const
+    
+    return 0;
+}
