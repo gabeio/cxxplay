7180189023958ddef96337a4bae846cdb5d00106
Gabe Del <me@gabe.io>
2013-09-11 03:14:16 -0400

    changed const.cc

--- const.cc
+++ const.cc
@@ -1,13 +1,24 @@
 #include <iostream>
 
+int fnc()
+{
+    return 512;
+}
+
 int main()
 {
     // const int i; // error not initialized.
     const int z = 5;
     
-    extern const bitSize = 512; // extern allows to be used extern(ally)...
+    // extern const int bitSize = fnc(); // extern allows to be used extern(ally)... ??
     
     // z = 0; // error can not change variable of a const
+    
+    int bit = 1;
+    const int &byte = bit; // constant reference (not variable)
+    
+    bit = 2; // on source change
+    std::cout << byte << std::endl; // reference changed also byte=2;
     
     return 0;
 }
